954ee72783b6856acc530a7a329a62bd593da501
Turdali <turdalihasanboyev144@gmail.com>
2025-05-21 18:47:34 +0500

    `Added Testimonial model and Category model with CategoryRole choices, updated SubEmail model`

--- kaira/models.py
+++ kaira/models.py
@@ -10,11 +10,42 @@ class BaseModel(models.Model):
 
 
 class SubEmail(BaseModel):
-    email = models.EmailField(max_length=100, unique=True, db_index=True, help_text='Email address')
+    email = models.EmailField(max_length=100, unique=True)
 
-    def __str__(self):
-        return f'{self.email}'
-    
     class Meta:
         verbose_name = 'Sub Email'
         verbose_name_plural = 'Sub Emails'
+
+    def __str__(self):
+        return f'{self.email}'
+
+
+class Testimonial(BaseModel):
+    author = models.CharField(max_length=150)
+    testimonial = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Testimonial'
+        verbose_name_plural = 'Testimonials'
+
+    def __str__(self):
+        return f'{self.author}'
+
+
+class CategoryRole(models.TextChoices):
+    BLOG = 'blog', 'Blog'
+    PRODUCT = 'product', 'Product'
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=150, unique=True)
+    role = models.CharField(max_length=10, choices=CategoryRole.choices)
+    image = models.ImageField(upload_to='category_images/', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{self.name}'

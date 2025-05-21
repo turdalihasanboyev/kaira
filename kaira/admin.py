d02dbe189130530e44129d9964e98aa394d47fa1
Turdali <turdalihasanboyev144@gmail.com>
2025-05-21 18:29:09 +0500

    `Added admin site customization and a base model with a sub email model.`

--- kaira/admin.py
+++ kaira/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+
+admin.site.site_header = 'kaira Admin Panel'
+admin.site.site_title = 'kaira Admin Panel'
+admin.site.index_title = 'Welcome to kaira Admin Panel'
--- kaira/models.py
+++ kaira/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SubEmail(BaseModel):
+    email = models.EmailField(max_length=100, unique=True, db_index=True, help_text='Email address')
+
+    def __str__(self):
+        return f'{self.email}'
+    
+    class Meta:
+        verbose_name = 'Sub Email'
+        verbose_name_plural = 'Sub Emails'
